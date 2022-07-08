361184e9683e1c7329eeb28c8a8da1886ec25115
asmeyb <asmeyb@gmail.com>
2022-07-08 15:05:19 +0300

    NavBar, SideBar & Footer

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import React, { useState } from "react";
+import { ImCancelCircle } from "react-icons/im";
+import { AiOutlineMenu, AiFillHome } from "react-icons/ai";
+import Link from "next/link";
+import GoogleLogin from "react-google-login";
+import Discover from "./Discover";
+import SuggestedAccounts from "./SuggestedAccounts";
+import Footer from "./Footer";
 
 const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
-  )
-}
+	const [showSidebar, setShowSidebar] = useState(true);
 
-export default Sidebar
+	const normalLink =
+		"flex items-center gap-3 hover:bg-primary p-3 justify-center xl:justify-start cursor-pointer font-semibold text-[#F51997] ROUNDED";
+
+	const userProfile = false;
+
+	return (
+		<div>
+			<div>
+				<div
+					className="block xl:hidden m-2 ml-4 mt-3 text-xl"
+					onClick={() => setShowSidebar((prev) => !prev)}
+				>
+					{showSidebar ? <ImCancelCircle /> : <AiOutlineMenu />}
+				</div>
+				{showSidebar && (
+					<div className="xl:w-400 w-20 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3">
+						<div className="xl:bottom-b-2 border-gray-200 xl:pb-4">
+							<Link href="/">
+								<div className={normalLink}>
+									<p className="text-2xl">
+										<AiFillHome />
+									</p>
+									<span className="text-xl hidden xl:block">For You</span>
+								</div>
+							</Link>
+						</div>
+						{!userProfile && (
+							<div className="px-2 py-4 hidden xl:block">
+								<p className="text-gray-400">
+									Log in to like and comment on videos
+								</p>
+								<div className="pr-4">
+									<GoogleLogin
+										clientId=""
+                                        render={(renderProps) => (
+											<button
+												onClick={renderProps.onClick}
+												disabled={renderProps.disabled}
+												className="bg-white text- cursor-pointer text-[#F51997] border-[1px] border-[#F51997] font-semibold px-6 py-3 rounded-md outline-none w-full mt-3 hover:text-white hover:bg-[#f51997]"
+											>
+												Log In
+											</button>
+										)}  
+										onSuccess={() => {}}
+										onFailure={() => {}}
+										cookiePolicy="single_host_origin"										                                                                          
+									/>
+								</div>
+							</div>
+						)}
+                        <Discover/>
+                        <SuggestedAccounts/>
+                        <Footer/>
+					</div>
+				)}
+			</div>
+		</div>
+	);
+};
+
+export default Sidebar;
